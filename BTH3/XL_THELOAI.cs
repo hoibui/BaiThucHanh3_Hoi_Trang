f086c956277f7d12ea2944766d2861658c352b5f
hoibui <athoibuiqt@gmail.com>
2017-09-18 16:21:49 +0700

    a

---
+++ BTH3/XL_THELOAI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTH3
+{
+    class XL_THELOAI:XuLy
+    {
+          #region cac ham kt
+        public XL_THELOAI() : base("NHAXUATBAN") { }
+        public XL_THELOAI(string pChuoi_SQL) : base("NHAXUATBAN", pChuoi_SQL) { }
+        #endregion
+        #region cc ham xu ly sk
+    /*    public void Tim(DataRow p_Dong_dieu_kien)
+    {
+        string chuoi_DK="";
+        ArrayList mang_DK = new ArrayList();
+        if (p_Dong_dieu_kien["MaNXB"] !=null)
+             mang_DK.Add("MaNXB LIKE '*"+ p_Dong_dieu_kien["MaNXB"]+"*'");
+        if (p_Dong_dieu_kien["TenNXB"] !=null)
+             mang_DK.Add("TenNXB LIKE '*"+ p_Dong_dieu_kien["TenNXB"]+"*'");
+        if (mang_DK.Count >0)
+        for(int i =0; i<mang_DK.Count; i++)
+        if (i == 0) chuoi_DK = mang_DK[i].ToString();
+        else chuoi_DK += " AND " + mang_DK[i];
+        Loc_du_lieu(chuoi_DK);
+    }*/
+        #endregion
+    }
+    
+}
--- BTH3/XuLy.cs
+++ BTH3/XuLy.cs
@@ -5,52 +5,147 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace BTH3
 {
     class XuLy : DataTable
     {
-        #region Bien cuc bo
+        #region bien cuc bo
         public static String Chuoi_lien_ket;
         private SqlDataAdapter mBo_doc_ghi = new SqlDataAdapter();
         private SqlConnection mKet_noi;
-        private String mChuoi_SQL;
-        public string chuoi_csdl;
-        private String mTen_bang; 
+        private string chuoi_csdl;   
+        private String mChuoi_SQL;     
+        private String mTen_bang;
         #endregion
 
         #region Cac thuoc tinh
-        public String Chuoi_SQL
+        public string Chuoi_csdl
+        {
+            get { return chuoi_csdl; }
+            set { chuoi_csdl = value; }
+        }
+        public String MChuoi_SQL
         {
             get { return mChuoi_SQL; }
             set { mChuoi_SQL = value; }
         }
-        public String Ten_bang
-        {
-            get { return mTen_bang; }
-            set { mTen_bang = value; }
-        }
         public int So_dong
         {
             get { return this.DefaultView.Count; }
-           
         }
         #endregion
-
         #region Cac phuong thuc khoi tao
-        public XuLy():base(){}
-        public XuLy(String pTenBang)
+        public XuLy() : base() { }
+        // tao moi mot bảng với pTEn_bang
+        public XuLy(String pTen_bang)
         {
-            mTen_bang=pTenBang;
+            mTen_bang = pTen_bang;
+            Doc_bang();
+        }
+        // tạo mới bảng với câu truy vấn
+        public XuLy(String pTen_bang, String pChuoi_SQL)
+        {
+            mTen_bang = pTen_bang;
+            mChuoi_SQL = pChuoi_SQL;
             Doc_bang();
         }
         #endregion
-        private void Doc_bang()
+        #region
+
+        public void Doc_bang()
         {
+            if (mChuoi_SQL == null)
+                mChuoi_SQL = "SELECT * FROM " + mTen_bang;
+            if (mKet_noi == null)
+                mKet_noi = new SqlConnection(Chuoi_lien_ket);
+            try
+            {
+                mBo_doc_ghi = new SqlDataAdapter(mChuoi_SQL, mKet_noi);
+                mBo_doc_ghi.FillSchema(this, SchemaType.Mapped);
+                mBo_doc_ghi.Fill(this);
+                mBo_doc_ghi.RowUpdated += new SqlRowUpdatedEventHandler(mBo_doc_ghi_RowUpdated);
+                SqlCommandBuilder Bo_phat_sinh = new SqlCommandBuilder(mBo_doc_ghi);
+            }
+            catch(SqlException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        public Boolean Ghi()
+        {
+            Boolean ket_qua = true;
+            try
+            {
+                this.AcceptChanges();
+            }
+            catch (SqlException )
+            {
+                this.RejectChanges();
+                ket_qua = false;
+            }
+            return ket_qua;
+        }
+        // lọc dữ liệu
+        public void Loc_du_lieu(String pDieu_kien)
+        {
+            try
+            {
+                this.DefaultView.RowFilter = pDieu_kien;
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+        }
+        public int Thuc_hien_lenh(String Lenh)
+        {
+            try
+            {
+                SqlCommand Cau_lenh = new SqlCommand(Lenh, mKet_noi);
+                mKet_noi.Open();
+                int ket_qua = Cau_lenh.ExecuteNonQuery();
+                mKet_noi.Close();
+                return ket_qua;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+        public Object Thuc_hien_lenh_tinh_toan(String Lenh)
+        {
+            try
+            {
+                SqlCommand Cau_lenh = new SqlCommand(Lenh, mKet_noi);
+                mKet_noi.Open();
+                Object ket_qua = Cau_lenh.ExecuteScalar();
+                mKet_noi.Close();
+                return ket_qua;
+            }
+            catch
+            {
+                return null;
+            }
             
         }
-        
-      
-    }
+        #endregion
+        #region xu lu su kien
 
+        private void mBo_doc_ghi_RowUpdated(Object sender,SqlRowUpdatedEventArgs e)
+        {
+            if(this.PrimaryKey[0].AutoIncrement)
+            {
+                if((e.Status==UpdateStatus.Continue)&&(e.StatementType==StatementType.Insert))
+                {
+                    SqlCommand cmd = new SqlCommand("Select @@IDENTITY ", mKet_noi);
+                    e.Row.ItemArray[0] = cmd.ExecuteScalar();
+                    e.Row.AcceptChanges();
+                }
+            }
+        }
+        #endregion
+    }
 }
